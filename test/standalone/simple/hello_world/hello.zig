7423d6a4044bd2c92ebfe5023bd4f3ad2789c793
Andrew Kelley <andrew@ziglang.org>
2025-12-19 17:38:19 -0800

    fix "hello world"
    14K -> 57K

--- test/standalone/simple/hello_world/hello.zig
+++ test/standalone/simple/hello_world/hello.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
+var static_single_threaded_io: std.Io.Threaded = .init_single_threaded;
+const io = static_single_threaded_io.ioBasic();
+
 pub fn main() !void {
-    try std.Io.File.stdout().writeAll("Hello, World!\n");
+    try std.Io.File.stdout().writeStreamingAll(io, "Hello, World!\n");
 }
