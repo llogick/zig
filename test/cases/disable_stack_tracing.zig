6dbc95fb1f3150eda3966215942f1d545b6bc2ac
Andrew Kelley <andrew@ziglang.org>
2025-12-23 22:14:09 -0800

    update test cases

--- test/cases/disable_stack_tracing.zig
+++ test/cases/disable_stack_tracing.zig
@@ -8,13 +8,13 @@ pub fn main() !void {
     var stdout = std.Io.File.stdout().writer(std.Options.debug_io, &buf);
 
     const captured_st = try foo(&stdout.interface, &st_buf);
-    try std.debug.writeStackTrace(&captured_st, &stdout.interface, .no_color);
+    try std.debug.writeStackTrace(&captured_st, .{ .writer = &stdout.interface, .mode = .no_color });
     try stdout.interface.print("stack trace index: {d}\n", .{captured_st.index});
 
     try stdout.interface.flush();
 }
 fn foo(w: *std.Io.Writer, st_buf: []usize) !std.builtin.StackTrace {
-    try std.debug.writeCurrentStackTrace(.{}, w, .no_color);
+    try std.debug.writeCurrentStackTrace(.{}, .{ .writer = w, .mode = .no_color });
     return std.debug.captureCurrentStackTrace(.{}, st_buf);
 }
 
