790d28d6cd9029805af857d56645a6ce191bfe91
Andrew Kelley <andrew@ziglang.org>
2026-01-16 12:30:24 -0800

    std.testing: delete refAllDeclsRecursive
    suggested alternatives:
    - actual tests
    - explicitly list the decls
    - compile an example application that uses the API
    - stop worrying about dead code
    - refAllDecls (non recursive) in each file
    
    Don't fight the laziness, embrace it.
    
    closes #23608
    closes #30813

--- lib/std/testing.zig
+++ lib/std/testing.zig
@@ -1200,21 +1200,6 @@ pub fn refAllDecls(comptime T: type) void {
     }
 }
 
-/// Given a type, recursively references all the declarations inside, so that the semantic analyzer sees them.
-/// For deep types, you may use `@setEvalBranchQuota`.
-pub fn refAllDeclsRecursive(comptime T: type) void {
-    if (!builtin.is_test) return;
-    inline for (comptime std.meta.declarations(T)) |decl| {
-        if (@TypeOf(@field(T, decl.name)) == type) {
-            switch (@typeInfo(@field(T, decl.name))) {
-                .@"struct", .@"enum", .@"union", .@"opaque" => refAllDeclsRecursive(@field(T, decl.name)),
-                else => {},
-            }
-        }
-        _ = &@field(T, decl.name);
-    }
-}
-
 pub const FuzzInputOptions = struct {
     corpus: []const []const u8 = &.{},
 };
